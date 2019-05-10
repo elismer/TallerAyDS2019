8a02bba9db35853ca077670e664a106907ba4d21
elismer <elismerbongiovanni@hotmail.com>
2019-05-10 11:05:11 -0300

    Borrado de foreings keys y cambio de id's foraneos

--- src/main/resources/triviaDataBase.sql
+++ src/main/resources/triviaDataBase.sql
@@ -10,7 +10,7 @@ CREATE TABLE if not exists users (
   UNIQUE (dni),
   created_at DATETIME,
   updated_at DATETIME
-  );
+);
   
 
 CREATE TABLE if not exists categories (
@@ -23,42 +23,35 @@ CREATE TABLE if not exists categories (
 CREATE TABLE if not exists levels (
   id int(11) NOT NULL auto_increment PRIMARY KEY,
   number_level ENUM ('1','2','3','4','5','6','7','8','9','10') NOT NULL,
-  id_cat int(11) NOT NULL,
+  categories_id int(11) NOT NULL,
   created_at DATETIME,
   updated_at DATETIME,
-  CONSTRAINT fklevel FOREIGN KEY (id_cat)
-	REFERENCES categories (id)
+)
   );
   
 
 CREATE TABLE if not exists questions(
   id int(11) NOT NULL auto_increment PRIMARY KEY,
   description VARCHAR(140) NOT NULL,
-  id_cate int(11) NOT NULL,
+  categories_id int(11) NOT NULL,
   created_at DATETIME,
   updated_at DATETIME,
-  CONSTRAINT fkquestion FOREIGN KEY (id_cate)
-	REFERENCES categories (id)
+)
   );
   
 
 CREATE TABLE if not exists comments (
   id  int(11) NOT NULL auto_increment PRIMARY KEY,
   description VARCHAR(140) NOT NULL,
-  id_user int(11) NOT NULL,
+  users_id int(11) NOT NULL,
   created_at DATETIME,
   updated_at DATETIME,
-  CONSTRAINT fkcomment FOREIGN KEY (id_user)
-	REFERENCES users (id)
-);
 
 CREATE TABLE IF NOT EXISTS options(
   id int(11) NOT NULL auto_increment PRIMARY KEY,
   description VARCHAR(50),
-  type ENUM('CORRECT','INCORRECT') default 'INCORRECT',
-  id_question int NOT NULL,
-  CONSTRAINT fkoptions FOREIGN KEY (id_question) 
-	REFERENCES questions (id), 
+  type ENUM('CORRECT','INCORRECT','UNKNOW') default 'UNKNOW',
+  questions_id int NOT NULL,
   created_at DATETIME,
   updated_at DATETIME
 )ENGINE=InnoDB;
@@ -67,30 +60,22 @@ CREATE TABLE IF NOT EXISTS options(
 
 CREATE TABLE IF NOT EXISTS games(
   id int(11) NOT NULL auto_increment PRIMARY KEY,
-  id_user int NOT NULL,
-  CONSTRAINT fkgame FOREIGN KEY (id_user) 
-  	REFERENCES users (id),
+  users_id int NOT NULL,
   created_at DATETIME,
   updated_at DATETIME
 )ENGINE=InnoDB;
 
 CREATE TABLE IF NOT EXISTS answers(
   id int(11) NOT NULL auto_increment PRIMARY KEY,
-  id_option int NOT NULL,
-  id_game int NOT NULL,
-  CONSTRAINT fkanswer_1 FOREIGN KEY (id_option) 
-  	REFERENCES options (id),
-  CONSTRAINT fkanswer_2 FOREIGN KEY (id_game) 
-  	REFERENCES games (id),
+  options_id int NOT NULL,
+  games_id int NOT NULL,
   created_at DATETIME,
   updated_at DATETIME
 )ENGINE=InnoDB;
 
 CREATE TABLE IF NOT EXISTS stats(
   id int(11) NOT NULL auto_increment PRIMARY KEY,
-  id_user int NOT NULL,
-  CONSTRAINT fkstat FOREIGN KEY (id_user) 
-  	REFERENCES users (id),
+  users_id int NOT NULL,
   created_at DATETIME,
   updated_at DATETIME
 )ENGINE=InnoDB;
